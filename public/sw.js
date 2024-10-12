e01bcb44f3f5f63c05f960dd7c53eb8aa1b3697b
shivam123p <kshivam12112000@gmail.com>
2024-10-12 20:29:07 +0530

    minor change4

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-restricted-globals */
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('v1').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/styles.css',
+          '/app.js',
+          // Add other assets you want to cache
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  /* eslint-enable no-restricted-globals */
