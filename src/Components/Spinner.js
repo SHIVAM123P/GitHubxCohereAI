c5d36be6ca15703396dda3268f4d3595be990a2a
shivam123p <kshivam12112000@gmail.com>
2024-12-05 23:40:54 +0530

    Fix Spinner

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { ScanSearch } from 'lucide-react';
 
 const Spinner = () => {
-  const [loadingText, setLoadingText] = useState('Scanning GitHub Profile...');
+  const [loadingText, setLoadingText] = useState('Scanning Your GitHub Profile...');
   const [dots, setDots] = useState('');
 
   useEffect(() => {
@@ -29,8 +29,8 @@ const Spinner = () => {
   }, []);
 
   return (
-    <div className="flex flex-col justify-center items-center  text-white"> 
-      <div className="relative w-24 h-20 mb-4">
+    <div className="flex flex-col justify-center items-center mt-12 text-white"> 
+      <div className="relative w-24 h-24 mb-4">
         {/* Outer neon glow */}
         <div className="absolute top-0 left-0 w-full h-full border-4 border-cyan-500 rounded-full animate-pulse opacity-100"></div>
         
@@ -38,8 +38,8 @@ const Spinner = () => {
         <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
           <ScanSearch
             className=" animate-spin-slow animate-pulse" 
-            size={48} 
-            // strokeWidth={1.5}
+            size={58} 
+            strokeWidth={1.5}
           />
         </div>
       </div>
