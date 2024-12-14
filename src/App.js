eed10fe23522f546fec23d42ddcb34fffab64613
shivam123p <kshivam12112000@gmail.com>
2024-12-14 20:40:17 +0530

    Fix

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ import "./Components/OpenSourceProjects.css";
 import SharedTwinBanner from './Components/SharedTwinBanner';
 
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-// const API_BASE_URL = 'https://gitstatsserver.onrender.com';
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = 'https://gitstatsserver.onrender.com';
+// const API_BASE_URL = "http://localhost:5000";
 const graphQLClient = new GraphQLClient("https://api.github.com/graphql", {
   headers: { authorization: `Bearer ${GITHUB_TOKEN}` },
 });
